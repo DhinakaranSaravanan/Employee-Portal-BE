30d8d5d52268b499cc9d0cec5cda0ebcc7b5f2c8
DhinakaranSaravanan <dhinakarannano@gmail.com>
2023-11-27 09:56:00 +0530

    err chnage

--- config/corsOption.js
+++ config/corsOption.js
@@ -1,7 +1,7 @@
 const whitelist = require('./allowedOrigin')
 const corsOption ={
     origin : (origin, callback) => {
-        if(whitelist.indexOf(origin) !== -1 /* || !origin */){
+        if(whitelist.indexOf(origin) !== -1 || !origin){
             callback(null, true)
         }else{
             callback(new Error("not allowed your cors request"))
